614418f7ec302a5319826310d9cfa071959d1b16
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 14:09:21 +0300

    url port

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import app from "./app.js";
 
 const server = app.listen(process.env.PORT, () => {
-    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+    console.log(`Server is running on https://portfolio-backend-six-orpin.vercel.app/`);
 })

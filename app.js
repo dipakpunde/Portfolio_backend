81815c383751dafc672fb7f9f3125f6abfdd260c
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 15:41:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -10,17 +10,17 @@ const app = express()
 
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
+const origin = 'https://portfolio-frontend-omega-five.vercel.app';
+
 app.use(cors({
-  origin: "https://portfolio-frontend-omega-five.vercel.app",
-  methods: ["GET", "POST", "OPTIONS"],
+  origin,
+  methods: ['GET','POST','OPTIONS'],
+  allowedHeaders: ['Content-Type'],
   credentials: true
 }));
 
-app.options("*", cors({
-  origin: "https://portfolio-frontend-omega-five.vercel.app",
-  methods: ["GET", "POST", "OPTIONS"],
-  credentials: true
-}));
+app.options('*', cors());
 app.use(cookieParser())
 
 app.use('/auth',authroutes);

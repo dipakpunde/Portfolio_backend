bb2cac0bc3a780930429a56f87671eaeadfb48dd
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 15:05:24 +0300

    tyy

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "portfolio-backend-six-orpin.vercel.app",
+    origin: "https://portfolio-frontend-2hi6.vercel.app",
     credentials: true
 }))
 app.use(cookieParser())

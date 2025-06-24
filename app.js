830c5fce85af4e57710bf28148fd3b71dd635dbf
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 12:51:35 +0300

    cors issue

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://dipak-punde-finacle-portfolio.netlify.app",
     credentials: true
 }))
 app.use(cookieParser())

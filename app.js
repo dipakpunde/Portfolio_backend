0ea67a74657ba339a7796d66fc31f1ae12c8a2f5
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 13:26:08 +0300

    cha

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "https://dipak-punde-finacle-portfolio.netlify.app",
+    origin: "http://localhost:3000",
     credentials: true
 }))
 app.use(cookieParser())

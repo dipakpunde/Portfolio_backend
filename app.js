4d3e3f81796d4fa7ba062430ea25f4adc641ef6a
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 14:02:51 +0300

    url

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://portfolio-frontend-seven-iota.vercel.app/",
     credentials: true
 }))
 app.use(cookieParser())

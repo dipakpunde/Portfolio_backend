95d2f0e55756852c232ec18149e19b947f825f23
Dipak Punde <dipakpunde2000@gmail.com>
2025-06-24 15:36:51 +0300

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors({
-    origin: "https://portfolio-backend-six-orpin.vercel.app",
+    origin: "https://portfolio-frontend-omega-five.vercel.app",
     credentials: true
 }))
 app.use(cookieParser())
